aa79c49ecf88b636d869758d2613f204fb1c6c2c
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-12 12:01:06 -0300

    feat: set up box composable

--- app/src/main/java/com/example/kotlinfiap/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfiap/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,3 +24,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun InitialScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Cyan)
+    ) {
+
+    }
+}
