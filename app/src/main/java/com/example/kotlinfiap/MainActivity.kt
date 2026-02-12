7ce0a9a01bb633b519c14dbf340e421863f66a52
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-12 15:29:12 -0300

    update: image alignment

--- app/src/main/java/com/example/kotlinfiap/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfiap/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -36,7 +37,9 @@ fun InitialScreen(){
     ) {
         Image(
             painter = painterResource(R.drawable.bot),
-            contentDescription = "Astro Bot posing"
+            contentDescription = "Astro Bot posing",
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
         )
 
     }
