3ff94306105ddd500aefa7de2dcde04492f936f9
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-12 15:19:21 -0300

    add: image asset and image rendering logic

--- app/src/main/java/com/example/kotlinfiap/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfiap/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
 
@@ -32,6 +34,10 @@ fun InitialScreen(){
             .fillMaxSize()
             .background(color = Color.Black)
     ) {
+        Image(
+            painter = painterResource(R.drawable.bot),
+            contentDescription = "Astro Bot posing"
+        )
 
     }
 }
