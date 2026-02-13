22c01f51e2c6c260ec2ee3cab180bdd3219f636a
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-13 12:31:19 -0300

    update: rendered buttons

--- app/src/main/java/com/example/kotlinfiap/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfiap/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -76,6 +78,18 @@ fun InitialScreen(){
                     fontWeight = FontWeight.Bold,
                     color = Color.White
                 )
+                Row{
+                    Button(
+                        onClick = {}
+                    ) {
+
+                    }
+                    Button(
+                        onClick = {}
+                    ) {
+
+                    }
+                }
             }
         }
     }
