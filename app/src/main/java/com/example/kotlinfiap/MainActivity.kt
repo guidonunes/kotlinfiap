ef92e372fc90aa789a736ef926959468c8231509
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-13 11:14:13 -0300

    add: new placeholder column

--- app/src/main/java/com/example/kotlinfiap/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfiap/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinfiap
 
 import android.os.Bundle
+import android.telecom.Connection
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,12 +10,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +43,7 @@ fun InitialScreen(){
     ) {
         Column(
             modifier = Modifier
+                .padding(16.dp)
                 .fillMaxSize()
                 .background(color = Color.Gray)
         ){
@@ -45,6 +51,13 @@ fun InitialScreen(){
                 painter = painterResource(R.drawable.bot),
                 contentDescription = "Astro Bot posing",
             )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .background(color = Color.Green)
+            ){}
         }
     }
 }
