4099ac3fbc94d623dcb2f9044d82550034551ab7
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-16 17:01:10 -0300

    fix: genymotion rendering issue

--- app/src/main/java/com/example/kotlinfiap/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfiap/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinfiapTheme {
-
+                InitialScreen()
             }
         }
     }
