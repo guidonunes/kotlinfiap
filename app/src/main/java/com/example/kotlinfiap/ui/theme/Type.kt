6450914125787dee44edcf521dedddd56b9bc4cd
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-17 10:25:05 -0300

    feat: set up fonts

--- app/src/main/java/com/example/kotlinfiap/ui/theme/Type.kt
+++ app/src/main/java/com/example/kotlinfiap/ui/theme/Type.kt
@@ -1,11 +1,21 @@
 package com.example.kotlinfiap.ui.theme
 
 import androidx.compose.material3.Typography
+import com.example.kotlinfiap.R
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+//Poppins
 
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold),
+
+
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
