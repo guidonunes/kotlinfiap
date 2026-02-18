a227643a279c7a9e224b203146275510804e3751
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-18 11:01:54 -0300

    update: extract text to string resource

--- app/src/main/java/com/example/kotlinfiap/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/kotlinfiap/screens/SignUpScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlinfiap.R
 import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
 
 @Composable
@@ -16,10 +18,10 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
 fun TitleComponent(modifier: Modifier = Modifier) {
     Column {
         Text(
-            text = "Sign Up"
+            text = stringResource(R.string.sign_up)
         )
         Text(
-            text = "Create New Account"
+            text = stringResource(R.string.create_account)
         )
     }
 }
