0f86f1cdcfeddcf1bafbb6e5bc626f38db25b7e9
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-18 09:54:33 -0300

    create: package and realocate initial screen code

---
+++ app/src/main/java/com/example/kotlinfiap/screens/InitialScreen.kt
@@ -0,0 +1,163 @@
+package com.example.kotlinfiap.screens
+
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kotlinfiap.R
+import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
+
+@Composable
+fun InitialScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = MaterialTheme
+                    .colorScheme
+                    .background
+            )
+    ) {
+        Card(
+            modifier = Modifier
+                .width(180.dp)
+                .height(90.dp)
+                .align(Alignment.TopEnd),
+            shape = RoundedCornerShape(
+                bottomStart = 85.dp
+            ),
+            colors = CardDefaults
+                .cardColors(
+                    containerColor = MaterialTheme
+                        .colorScheme
+                        .primary
+                )
+
+        ) {}
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(
+                painter = painterResource(R.drawable.bot),
+                contentDescription = "Astro Bot posing",
+                modifier = Modifier
+                    .size(240.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                Text(
+                    text = stringResource(R.string.app_subtitle),
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+                Text(
+                    text = stringResource(R.string.app_title),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier
+                        .padding(top =14.dp, bottom = 18.dp),
+                    textAlign = TextAlign.Center
+                )
+
+                Row{
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults
+                            .buttonColors(
+                                containerColor = MaterialTheme.colorScheme.tertiary
+                            ),
+                        border = BorderStroke(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.onSurface
+                        ),
+
+                        ) {
+                        Text(
+                            text = stringResource(R.string.button_red_pill),
+                            color = MaterialTheme.colorScheme.onPrimary,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults
+                            .buttonColors(
+                                containerColor = MaterialTheme.colorScheme.primary
+                            ),
+                        border = BorderStroke(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.onSurface
+                        ),
+                    ) {
+                        Text(
+                            text = stringResource(R.string.button_blue_pill) ,
+                            color = MaterialTheme.colorScheme.onPrimary,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+                    }
+                }
+            }
+        }
+        Card(
+            modifier = Modifier
+                .width(180.dp)
+                .height(90.dp)
+                .align(Alignment.BottomStart),
+            shape = RoundedCornerShape(
+                topEnd = 85.dp
+            ),
+            colors = CardDefaults
+                .cardColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+        ) {}
+    }
+}
+
+@Composable
+@Preview(
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+fun InitialScreenPreview() {
+    KotlinfiapTheme {
+        InitialScreen()
+    }
+}
