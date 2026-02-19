4fdeb22085ca8a2e51af1dc2c5ef92a426852770
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-19 09:20:52 -0300

    update: top and bottom cards

--- app/src/main/java/com/example/kotlinfiap/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/kotlinfiap/screens/SignUpScreen.kt
@@ -3,6 +3,7 @@ package com.example.kotlinfiap.screens
 import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -34,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kotlinfiap.R
@@ -44,14 +46,19 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(
+                MaterialTheme.colorScheme.background
+            )
     ) {
-
+        TopEndCard(modifier = Modifier.align(alignment = Alignment.TopEnd))
+        BottomStartCard(modifier = Modifier.align(alignment = Alignment.BottomStart))
     }
 }
 
 @Preview (
     showBackground = true,
-    showSystemUi = true
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Composable
 private fun SignUpScreenPreview () {
