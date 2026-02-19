896986ff51123c48d4c051a178cc07dfa693ecf7
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-19 12:04:59 -0300

    styling: assembled login components

--- app/src/main/java/com/example/kotlinfiap/screens/LoginScreen.kt
+++ app/src/main/java/com/example/kotlinfiap/screens/LoginScreen.kt
@@ -1,7 +1,13 @@
 package com.example.kotlinfiap.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,15 +16,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.RemoveRedEye
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,11 +38,29 @@ import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
 
 @Composable
 fun LoginScreen (modifier: Modifier = Modifier) {
-    
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        TopEndCard(modifier = Modifier.align(alignment = Alignment.TopEnd))
+        BottomStartCard(modifier = Modifier.align(alignment = Alignment.BottomStart))
+        Column(
+            modifier = Modifier
+                .padding(32.dp)
+                .fillMaxWidth()
+                .align(alignment = Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LoginTitle()
+            LogInForm()
+        }
+    }
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun LoginScreenPreview () {
     KotlinfiapTheme() {
@@ -90,7 +118,7 @@ fun LogInForm(modifier: Modifier = Modifier) {
                 ),
             label = {
                 Text(
-                    text = "Your Email",
+                    text = stringResource(R.string.your_email_login),
                     style = MaterialTheme.typography.labelSmall
                 )
             },
@@ -121,7 +149,7 @@ fun LogInForm(modifier: Modifier = Modifier) {
                 ),
             label = {
                 Text(
-                    text = "Your Password",
+                    text = stringResource(R.string.your_password_login),
                     style = MaterialTheme.typography.labelSmall
                 )
             },
@@ -145,6 +173,48 @@ fun LogInForm(modifier: Modifier = Modifier) {
             )
 
         )
+        Spacer(modifier = Modifier.padding(24.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = ButtonDefaults
+                .buttonColors(
+                    containerColor = MaterialTheme.colorScheme.tertiary
+                ),
+            border = BorderStroke(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSurface
+            ),
+
+            ) {
+            Text(
+                text = stringResource(R.string.sign_in),
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        Spacer(modifier = Modifier.padding(8.dp))
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Don't have an account?",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+            TextButton(
+                onClick = {}
+            ) {
+                Text(
+                    text = stringResource(R.string.sign_up),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
 
     }
 }
