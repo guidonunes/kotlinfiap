cb15e80fd9585ec813f6e49860b188abec8e21a0
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-19 11:14:34 -0300

    feat: update email input field

--- app/src/main/java/com/example/kotlinfiap/screens/LoginScreen.kt
+++ app/src/main/java/com/example/kotlinfiap/screens/LoginScreen.kt
@@ -4,12 +4,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kotlinfiap.R
@@ -64,5 +73,47 @@ private fun LogInTitlePreview() {
 
 @Composable
 fun LogInForm(modifier: Modifier = Modifier) {
+    Column() {
+        OutlinedTextField(
+            value="",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults
+                .colors(
+                    focusedBorderColor = MaterialTheme.colorScheme.primary,
+                    unfocusedBorderColor = MaterialTheme.colorScheme.primary
+                ),
+            label = {
+                Text(
+                    text = "Your Email",
+                    style = MaterialTheme.typography.labelSmall
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Email icon",
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
+
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun LogInFormPreview() {
+    KotlinfiapTheme() {
+        LogInForm()
+    }
     
 }
