38e1b7bbf7095deb41fc3827d0bc19e7907507d0
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-20 10:00:07 -0300

    styles: updated top bar layout

--- app/src/main/java/com/example/kotlinfiap/screens/HomeScreen.kt
+++ app/src/main/java/com/example/kotlinfiap/screens/HomeScreen.kt
@@ -1,13 +1,25 @@
 package com.example.kotlinfiap.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlinfiap.R
 import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
 
 @Composable
@@ -37,5 +49,46 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 private fun HomeScreenPreview() {
     KotlinfiapTheme() {
         HomeScreen()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {
+            Row() {
+                Column() {
+                    Text(
+                        text = "Hello, John Doe!",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "johndoe@email.com",
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                }
+                Card(
+                    shape = CircleShape,
+                    colors = CardDefaults
+                        .cardColors()
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.profile),
+                        contentDescription = "User image"
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun MyTopAppBarPreview() {
+    KotlinfiapTheme() {
+        MyTopAppBar()
     }
 }
--- app/src/main/java/com/example/kotlinfiap/ui/theme/Type.kt
+++ app/src/main/java/com/example/kotlinfiap/ui/theme/Type.kt
@@ -25,6 +25,20 @@ val Typography = Typography(
         lineHeight = 68.sp,
         letterSpacing = 0.5.sp
     ),
+    displayMedium = TextStyle(
+        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 64.sp,
+        lineHeight = 68.sp,
+        letterSpacing = 0.5.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 100.sp,
+        letterSpacing = 0.5.sp
+    ),
     titleLarge = TextStyle(
         fontFamily = poppinsFamily,
         fontWeight = FontWeight.SemiBold,
