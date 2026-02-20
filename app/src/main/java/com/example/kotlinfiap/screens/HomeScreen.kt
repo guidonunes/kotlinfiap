f976392d5da644660fddb0b31fbc939bca996232
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-20 10:30:52 -0300

    styles: fix padding and bar height

--- app/src/main/java/com/example/kotlinfiap/screens/HomeScreen.kt
+++ app/src/main/java/com/example/kotlinfiap/screens/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
@@ -18,6 +19,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -62,14 +64,16 @@ private fun HomeScreenPreview() {
 fun MyTopAppBar(modifier: Modifier = Modifier) {
     TopAppBar(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .height(90.dp),
 
         title = {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Column() {
                     Text(
