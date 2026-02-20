3a6ebfad86c98c01156acfa739477c387cdf9464
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-20 10:25:59 -0300

    styles: updated top bar layout

--- app/src/main/java/com/example/kotlinfiap/screens/HomeScreen.kt
+++ app/src/main/java/com/example/kotlinfiap/screens/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinfiap.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
@@ -16,9 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kotlinfiap.R
 import com.example.kotlinfiap.ui.theme.KotlinfiapTheme
 
@@ -29,7 +34,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             .fillMaxSize()
     ) {
         Scaffold(
-            topBar = {},
+            topBar = {MyTopAppBar()},
             bottomBar = {},
             floatingActionButton = {},
 
@@ -56,8 +61,16 @@ private fun HomeScreenPreview() {
 @Composable
 fun MyTopAppBar(modifier: Modifier = Modifier) {
     TopAppBar(
+        modifier = Modifier
+            .fillMaxWidth(),
+
         title = {
-            Row() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 Column() {
                     Text(
                         text = "Hello, John Doe!",
@@ -73,7 +86,14 @@ fun MyTopAppBar(modifier: Modifier = Modifier) {
                 Card(
                     shape = CircleShape,
                     colors = CardDefaults
-                        .cardColors()
+                        .cardColors(
+                            containerColor = Color.Transparent
+                        ),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.primary
+
+                    )
                 ) {
                     Image(
                         painter = painterResource(R.drawable.profile),
