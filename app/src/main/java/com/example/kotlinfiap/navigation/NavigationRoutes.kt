09f110376c0675982e1eee6aa1afcb1a428ededc
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-23 09:59:34 -0300

    feat: implement navigation routes composable structure

--- app/src/main/java/com/example/kotlinfiap/navigation/NavigationRoutes.kt
+++ app/src/main/java/com/example/kotlinfiap/navigation/NavigationRoutes.kt
@@ -1,2 +1,17 @@
 package com.example.kotlinfiap.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavigationRoutes() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Destination.HomeScreen
+    ) {
+        composable(){}
+    }
+}
