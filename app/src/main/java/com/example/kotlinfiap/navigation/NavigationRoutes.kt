a10cf157ea04ae9df531c12e13815e0608f4dbc4
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-23 10:03:47 -0300

    feat: set up initial screen route

--- app/src/main/java/com/example/kotlinfiap/navigation/NavigationRoutes.kt
+++ app/src/main/java/com/example/kotlinfiap/navigation/NavigationRoutes.kt
@@ -4,14 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.kotlinfiap.screens.InitialScreen
 
 @Composable
 fun NavigationRoutes() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = Destination.HomeScreen
+        startDestination = Destination.InitialScreen
     ) {
-        composable(){}
+        composable(Destination.InitialScreen.route){
+            InitialScreen()
+        }
     }
 }
