858ce7b6d5b202cac720ac6c7906edf62f2d653b
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-24 11:35:35 -0300

    feat: set up data class

--- app/src/main/java/com/example/kotlinfiap/model/Category.kt
+++ app/src/main/java/com/example/kotlinfiap/model/Category.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinfiap.model
 
 import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+import com.example.kotlinfiap.R
 
 data class Category(
     val id: Int = 0,
     val name: String = "",
-    @DrawableRes val image: Int? = 0
+    @DrawableRes val image: Int? = R.drawable.no_image,
+    val background: Color = Color.Gray
 )
