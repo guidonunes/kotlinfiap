d221dba7e9bfa3aa3a7dfad54abfc8e6397cc66d
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-24 11:25:15 -0300

    style: add icons for the row section

---
+++ app/src/main/java/com/example/kotlinfiap/model/Category.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinfiap.model
+
+import androidx.annotation.DrawableRes
+
+data class Category(
+    val id: Int = 0,
+    val name: String = "",
+    @DrawableRes val image: Int = 0
+)
