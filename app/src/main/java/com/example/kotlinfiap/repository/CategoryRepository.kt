2f4d5785b551a3b8c26e55c7a3f60dff022077a5
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-25 11:31:33 -0300

    update: category id sequence

--- app/src/main/java/com/example/kotlinfiap/repository/CategoryRepository.kt
+++ app/src/main/java/com/example/kotlinfiap/repository/CategoryRepository.kt
@@ -6,43 +6,43 @@ import com.example.kotlinfiap.model.Category
 
 fun getAllCategories() = listOf<Category>(
     Category(
-        id = 1,
+        id = 1000,
         name = "Action",
         image = R.drawable.action,
         background = Color(0xffabf2e9)
     ),
     Category(
-        id = 2,
+        id = 2000,
         name = "Adventure",
         image = R.drawable.adventure,
         background = Color(0xfff4d6c0)
     ),
     Category(
-        id = 3,
+        id = 3000,
         name = "RPG",
         image = R.drawable.rpg,
         background = Color(0xffc6dafa)
     ),
     Category(
-        id = 4,
+        id = 4000,
         name = "Simulation",
         image = R.drawable.simulation,
         background = Color(0xfff8d9d9)
     ),
     Category(
-        id = 5,
+        id = 5000,
         name = "Racing",
         image = R.drawable.racing,
         background = Color(0xffabf2e8)
     ),
     Category(
-        id = 6,
+        id = 6000,
         name = "Puzzle",
         image = R.drawable.puzzle,
         background = Color(0xff72412b)
     ),
     Category(
-        id = 7,
+        id = 7000,
         name = "Arcade",
         image = R.drawable.arcade,
         background = Color(0xff80deea)
