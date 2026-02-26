7da434d501a0fb3a796479a714b29a49e6eccd86
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-26 14:54:23 -0300

    update: add shared prefences to manage authentication

--- app/src/main/java/com/example/kotlinfiap/repository/UserRepository.kt
+++ app/src/main/java/com/example/kotlinfiap/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package com.example.kotlinfiap.repository
 import com.example.kotlinfiap.model.User
 
 interface UserRepository {
-    fun save(user: User)
+    fun saveUser(user: User)
     fun getUser(): User
     fun getUserByEmail(email: String): User?
     fun login(email: String, password: String): Boolean
