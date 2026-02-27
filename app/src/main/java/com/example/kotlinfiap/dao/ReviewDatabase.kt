6b5325b8cb5d673633d4e5967af0ec15f0925345
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-27 11:59:08 -0300

    feat: create review database representation

---
+++ app/src/main/java/com/example/kotlinfiap/dao/ReviewDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.kotlinfiap.dao
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.kotlinfiap.model.Review
+import com.example.kotlinfiap.model.User
+
+@Database(
+    entities = [User::class, Review::class],
+    version = 1,
+)
+abstract class ReviewDatabase: RoomDatabase() {
+    abstract fun userDao(): UserDao
+    companion object {
+        private lateinit var instance:  ReviewDatabase
+        fun getDatabase(context: Context): ReviewDatabase {
+            if(!::instance.isInitialized) {
+                instance = Room
+                    .databaseBuilder(
+                        context,
+                        ReviewDatabase::class.java,
+                        "review_database"
+                    )
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+            return instance
+        }
+
+    }
+
+}
+
