6925bd1c773d0b7e8d88851d7fab7bf3ef4a95a1
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-27 11:25:58 -0300

    feat: implement dao interface

---
+++ app/src/main/java/com/example/kotlinfiap/dao/UserDao.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinfiap.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.kotlinfiap.model.User
+
+
+@Dao
+interface UserDao {
+    @Insert
+    fun save(user: User): Int
+
+    @Delete
+    fun delete(user: User): Int
+
+    @Update
+    fun update(user: User): Int
+
+    @Query("SELECT * FROM tb_user WHERE id = :id")
+    fun getUserById(id: Int): User?
+
+    @Query("SELECT * FROM tb_user WHERE email = :email")
+    fun getUserByEmail(email: String): User?
+
+    @Query("SELECT * FROM tb_user WHERE email = :email AND password = :password")
+    fun login(email: String, password: String): User?
+
+}
