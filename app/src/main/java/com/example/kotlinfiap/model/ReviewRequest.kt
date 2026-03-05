c572e5a5725b8a50dde4ea967a3907212d374888
Guilherme Nunes <guilherme.augd@gmail.com>
2026-03-05 11:09:43 -0300

    feat: review request model

---
+++ app/src/main/java/com/example/kotlinfiap/model/ReviewRequest.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinfiap.model
+
+data class ReviewRequest(
+    val title: String = "",
+    val difficultyLevel: DifficultyLevel = DifficultyLevel.EASY,
+    val description: String = "",
+    val cookingTime: Int = 0,
+    val creationDate: String = "",
+    val category: Category = Category(id = 1),
+)
